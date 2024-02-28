357fd08f252114c2fed72ceb4e12f6209619525d
istvan.herbak@gmail.com <istvan.herbak@gmail.com>
2024-02-28 19:00:17 +0100

    init

---
+++ node_modules/stencil-package/stencil.config.ts
@@ -0,0 +1,31 @@
+import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target';
+
+export const config: Config = {
+  namespace: 'a0vy',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    angularOutputTarget({
+      componentCorePackage: 'stencil-package',
+      outputType: 'component',
+      directivesProxyFile: '../angular-workspace/projects/components/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/components/src/lib/stencil-generated/index.ts',
+    })
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
